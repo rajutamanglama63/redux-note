1b80c36c07fbf46e95963ecd2bed7f65fb1662e4
su-de-sh <sudesh7443@gmail.com>
2022-08-22 16:05:51 +0545

    first commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-
+//this is the sample change
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
