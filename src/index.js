7245f4bc7e35c3293fa37cb12655a5bc615cbb7b
su-de-sh <sudesh7443@gmail.com>
2022-08-22 16:47:31 +0545

    feat: display  note and toggle importance

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-//this is the sample change
+import {createStore} from 'redux';
+import noteReducer from './reducers/noteReducer';
+import {Provider} from "react-redux"
+
+
+const store = createStore(noteReducer)
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
