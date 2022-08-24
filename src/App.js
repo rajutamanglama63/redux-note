78f754fc1584baa2e817dece226e91b4ce7460a3
su-de-sh <sudesh7443@gmail.com>
2022-08-24 14:37:14 +0545

    feat: communicate redux with backend

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from "react";
-
+import { useEffect } from "react";
 import NewNote from "./components/NewNote";
 import Notes from "./components/Notes";
 import VisibilityFilter from "./components/VisibilityFilter";
+import noteServices from "./services/notes";
+import { setNotes } from "./reducers/noteReducer";
+import { useDispatch } from "react-redux";
 
 function App() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    noteServices.getAll().then((notes) => dispatch(setNotes(notes)));
+  }, [dispatch]);
+
   return (
     <div>
       <NewNote />
